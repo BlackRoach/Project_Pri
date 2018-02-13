459278d1cff463ee846d5b6009868f9808c057df
BlackRoach <black4roach@gmail.com>
2018-02-13 15:30:33 +0900

    add TestScript

---
+++ Project_Pri/Assets/Script/TestScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // 이건 테스트용 스크립트입니다.
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
