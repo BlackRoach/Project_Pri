7a506d455b1946efa07846a576432585f9fd1d24
BlackRoach <black4roach@gmail.com>
2018-02-13 23:26:19 +0900

    add TitleScene and make titlebuttons, logo function.

---
+++ Project_Pri/Assets/Script/FadeScript.cs
@@ -0,0 +1,54 @@
+﻿ using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeScript : MonoBehaviour {
+
+    [SerializeField] private Image fade;
+    [SerializeField] private GameObject TitleWindow;
+    [SerializeField] private GameObject IntroWindow;
+    public float playingTime = 0.5f;
+    public float WaitTime = 1.0f;
+    private float time = 0f;
+
+	void Start () {
+        IntroWindow.SetActive(true);
+        TitleWindow.SetActive(false);
+        StartCoroutine(FadeIn());
+ 
+    }
+    IEnumerator FadeIn()
+    {
+        Color color = fade.color;
+        time = 0f;
+        color.a = Mathf.Lerp(1f, 0f, time);
+        while (color.a > 0f)
+        {
+            time += Time.deltaTime / playingTime;
+            color.a = Mathf.Lerp(1f, 0f, time);
+            fade.color = color;
+            yield return null;
+        }
+        yield return new WaitForSeconds(WaitTime);
+        StartCoroutine(FadeOut());
+
+    }
+    IEnumerator FadeOut()
+    {
+        Color color = fade.color;
+        time = 0f;
+        color.a = Mathf.Lerp(0f, 1f, time);
+        while (color.a < 1f)
+        {
+            time += Time.deltaTime / playingTime;
+            color.a = Mathf.Lerp(0f, 1f, time);
+            fade.color = color;
+            yield return null;
+        }
+        yield return new WaitForSeconds(WaitTime);
+        this.gameObject.GetComponent<FadeScript>().enabled = false;
+        IntroWindow.SetActive(false);
+        TitleWindow.SetActive(true);
+    }
+   
+}
---
+++ Project_Pri/Assets/Script/Manager/TitleButtonManager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleButtonManager : MonoBehaviour {
+
+    // 싱글톤 패턴
+    private static TitleButtonManager instance = null;
+    public static TitleButtonManager Instance
+    {
+        get
+        {
+            if (instance)
+                return instance;
+            else
+                return instance = new GameObject("*Manager").AddComponent<TitleButtonManager>();
+        }
+    }
+    private void Awake()
+    {
+        instance = this;
+    }
+    [SerializeField] private GameObject selectableButtons;
+    [SerializeField] private GameObject touchToStartButton;
+
+    [SerializeField] private float playingTime = 1f;
+
+
+    private Image touchToStartImg;
+    private float time = 0f;
+    private bool isTitle = false;
+    void Start() {
+        touchToStartButton.SetActive(true);
+        selectableButtons.SetActive(false);
+
+        touchToStartImg = touchToStartButton.GetComponent<Image>();
+        StartCoroutine(TouchButtonInvisible());
+    }
+
+
+    void Update() {
+
+    }
+
+    public void TouchToStartButtonOn()
+    {
+        isTitle = true;
+        touchToStartButton.SetActive(false);
+        selectableButtons.SetActive(true);
+    }
+    public void NewGameButtonOn()
+    {
+        // 새게임 버튼
+        //초기화 진행
+        SceneManager.LoadScene("");
+    }
+    public void LoadGameButtonOn()
+    {
+        // 이어하기 버튼
+        SceneManager.LoadScene("");
+    }
+    public void CgModeButtonOn()
+    {
+        // cg모드 버튼
+    }
+    public void OptionButtonOn()
+    {
+        // 옵션 버튼
+    }
+    public void ExitButtonOn()
+    {
+        // 끝내기 버튼
+        Application.Quit();
+    }
+ 
+    IEnumerator TouchButtonInvisible()
+    {
+        Color color = touchToStartImg.color;
+        time = 0f;
+        color.a = Mathf.Lerp(1f, 0.5f, time);
+        while (color.a > 0.5f)
+        {
+            time += Time.deltaTime / playingTime;
+            color.a = Mathf.Lerp(1f, 0.5f, time);
+            touchToStartImg.color = color;
+            yield return null;
+        }
+        yield return null;
+        if (!isTitle)
+            StartCoroutine(TouchButtonVisible());
+    }
+    IEnumerator TouchButtonVisible()
+    {
+        Color color = touchToStartImg.color;
+        time = 0f;
+        color.a = Mathf.Lerp(0.5f, 1f, time);
+        while (color.a < 1f)
+        {
+            time += Time.deltaTime / playingTime;
+            color.a = Mathf.Lerp(0.5f, 1f, time);
+            touchToStartImg.color = color;
+            yield return null;
+        }
+        yield return null;
+        if(!isTitle)
+            StartCoroutine(TouchButtonInvisible());
+    }
+}
