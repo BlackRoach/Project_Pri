161f610c6ff19dc559d45e00c7df034cb4f48ceb
DESKTOP-VRCP7O8\jeong <hyeongman88@gmail.com>
2018-05-17 00:30:52 +0900

    Hyeongman_2_Week_Project is done!

---
+++ Project_Pri/Assets/Script/JHM_Scripts/DialogueText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class DialogueText  {
+
+
+
+    [TextArea(2,15)]
+    public string[] textFiles_01;
+
+
+} // class
+
+
+
+
+
+
+
+
+
+
---
+++ Project_Pri/Assets/Script/JHM_Scripts/TextBoxController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TextBoxController : MonoBehaviour {
+
+    public GameObject dialogueBox;
+
+    public Text text_Line;
+
+    public DialogueText textDialogues;
+
+
+    public List<string> textArray = new List<string>();
+
+    private int currentText;
+    [SerializeField]
+    private int endText;
+
+
+
+    private void Start()
+    {
+        foreach(string text in textDialogues.textFiles_01){
+            textArray.Add(text);
+        }
+
+        currentText = 0;
+        endText = textArray.Count - 1;
+    }
+
+    // text 작동 메소드
+    public void DialogueText()
+    {
+        if(currentText > textArray.Count - 1)
+        {
+            StopCoroutine(AutoTyping());
+            dialogueBox.SetActive(false);
+        }
+        if (currentText <= textArray.Count - 1)
+        {
+            text_Line.text = " ";
+
+            StartCoroutine(AutoTyping());
+
+            currentText++;
+        }
+    }
+    // text 줄줄이 나오도록 하는 메소드
+    IEnumerator AutoTyping()
+    {
+        foreach (char letter in textArray[currentText].ToCharArray())
+        {
+            text_Line.text += letter;
+
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+
+
+
+} // class
+
+
+
+
+
+
+
+
+
+
