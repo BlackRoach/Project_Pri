433bab51ac23d2c5e074259ef1a25de1070421b9
DESKTOP-H33RJBD\jeong <hyeongman88@gmail.com>
2018-06-15 03:20:24 +0900

    형만_5주차_완

--- Project_Pri/Assets/JHM.Assets/JHM_Scripts/Json_Controller.cs
+++ Project_Pri/Assets/JHM.Assets/JHM_Scripts/Json_Controller.cs
@@ -3,23 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using LitJson;
-
+using UnityEngine.UI;
 
 public class Json_Controller : MonoBehaviour {
 
+    public GameObject origin_Panel;
+    public GameObject condition_Panel;
+
+    public Text[] text_Condition;
+
     private string json_File_1;
-    private JsonData json_Data;
+
+    private JsonData load_Data;
+
+    private void Start()
+    {
+        origin_Panel.SetActive(true);
+        condition_Panel.SetActive(false);
+    }
 
     public void Load_Json_Files()
     {
         json_File_1 = File.ReadAllText(Application.dataPath + "/JHM.Assets/Resources/JsonFile01.json");
 
-        json_Data = JsonMapper.ToObject(json_File_1);
 
-        Debug.Log(json_Data);
+        load_Data = JsonMapper.ToObject(json_File_1);
 
+        Json_Parsing_Data(load_Data);
     }
     
+    private void Json_Parsing_Data(JsonData data)
+    {
+        origin_Panel.SetActive(false);
+        condition_Panel.SetActive(true);
+
+        // --------------------------
+
+        text_Condition[0].text = data[0]["START_STR"].ToString();
+        text_Condition[1].text = data[0]["START_CON"].ToString(); 
+        text_Condition[2].text = data[0]["START_HP"].ToString();
+
+    }
 
     
 } // class
