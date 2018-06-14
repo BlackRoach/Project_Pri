cdcecc9a7b0296879488b47c94f0fe4e4066b0b0
DESKTOP-H33RJBD\jeong <hyeongman88@gmail.com>
2018-06-15 02:57:00 +0900

    형만_5주차_01

---
+++ Project_Pri/Assets/JHM.Assets/JHM_Scripts/Json_Controller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using LitJson;
+
+
+public class Json_Controller : MonoBehaviour {
+
+    private string json_File_1;
+    private JsonData json_Data;
+
+    public void Load_Json_Files()
+    {
+        json_File_1 = File.ReadAllText(Application.dataPath + "/JHM.Assets/Resources/JsonFile01.json");
+
+        json_Data = JsonMapper.ToObject(json_File_1);
+
+        Debug.Log(json_Data);
+
+    }
+    
+
+    
+} // class
+
+
+
+
+
+
+
+
+
