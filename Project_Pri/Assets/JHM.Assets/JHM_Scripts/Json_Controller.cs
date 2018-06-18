39c79f783beedeba948a9149c0b44a00ed49ed94
DESKTOP-U6PLBE9\jeong <hyeongman88@gmail.com>
2018-06-19 04:53:58 +0900

    형만_5주차_수정_완

--- Project_Pri/Assets/JHM.Assets/JHM_Scripts/Json_Controller.cs
+++ Project_Pri/Assets/JHM.Assets/JHM_Scripts/Json_Controller.cs
@@ -11,11 +11,12 @@ public class Json_Controller : MonoBehaviour {
     public GameObject condition_Panel;
 
     public Text[] text_Condition;
-
-    private string json_File_1;
-
+    
     private JsonData load_Data;
 
+    //private string json_File_1;
+
+    private TextAsset json_File_1;
     private void Start()
     {
         origin_Panel.SetActive(true);
@@ -24,25 +25,24 @@ public class Json_Controller : MonoBehaviour {
 
     public void Load_Json_Files()
     {
-        json_File_1 = File.ReadAllText(Application.dataPath + "/JHM.Assets/Resources/JsonFile01.json");
+        origin_Panel.SetActive(false);
+        condition_Panel.SetActive(true);
 
+        // --------------------------
 
-        load_Data = JsonMapper.ToObject(json_File_1);
+        // json_File_1 = File.ReadAllText(Application.dataPath + "/JHM.Assets/Resources/JsonFile01.json");\
+        json_File_1 = Resources.Load<TextAsset>("JHM.Resources.Json/JsonFile01");
+
+        load_Data = JsonMapper.ToObject(json_File_1.text);
 
         Json_Parsing_Data(load_Data);
     }
     
     private void Json_Parsing_Data(JsonData data)
     {
-        origin_Panel.SetActive(false);
-        condition_Panel.SetActive(true);
-
-        // --------------------------
-
         text_Condition[0].text = data[0]["START_STR"].ToString();
         text_Condition[1].text = data[0]["START_CON"].ToString(); 
         text_Condition[2].text = data[0]["START_HP"].ToString();
-
     }
 
     
