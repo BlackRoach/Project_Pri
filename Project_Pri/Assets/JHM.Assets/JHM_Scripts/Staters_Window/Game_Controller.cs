18df8a2fde75a22a85472a518c743d58dedd947d
DESKTOP-1CKJN7Q\jeong <hyeongman88@gmail.com>
2018-06-22 04:10:09 +0900

    형만_6주차_프로젝트_03

--- Project_Pri/Assets/JHM.Assets/JHM_Scripts/Staters_Window/Game_Controller.cs
+++ Project_Pri/Assets/JHM.Assets/JHM_Scripts/Staters_Window/Game_Controller.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 
 public class Game_Controller : MonoBehaviour {
@@ -9,7 +10,11 @@ public class Game_Controller : MonoBehaviour {
 
     public GameObject origin_Panel;
     public GameObject condition_Panel;
+    public GameObject save_List_Panel;
+    public GameObject load_List_Panel;
+    public GameObject save_Description;
 
+    public Text text_Save_Location_Number;
 
     private int count_One;
     
@@ -29,6 +34,9 @@ public class Game_Controller : MonoBehaviour {
     {
         origin_Panel.SetActive(true);
         condition_Panel.SetActive(false);
+        save_List_Panel.SetActive(false);
+        save_Description.SetActive(false);
+        load_List_Panel.SetActive(false);
 
         // -------------------------
 
@@ -59,10 +67,25 @@ public class Game_Controller : MonoBehaviour {
 
         Json_Controller.instance.Defualt_Json_Data();
     }
+    public void Clicked_Button_Load_List_Panel()
+    {
+        load_List_Panel.SetActive(true);
+    }
     
+    public void Clicked_Button_Save_List_Panel()
+    {
+        save_List_Panel.SetActive(true);
+    }
 
+    public void Clicked_Button_Save_Description_Panel(int numberOfLocation)
+    {
+        save_List_Panel.SetActive(false);
+        save_Description.SetActive(true);
 
+        // -----------------------
 
+        text_Save_Location_Number.text = numberOfLocation.ToString();
+    }
 
 
 
--- Project_Pri/Assets/JHM.Assets/JHM_Scripts/Staters_Window/Json_Controller.cs
+++ Project_Pri/Assets/JHM.Assets/JHM_Scripts/Staters_Window/Json_Controller.cs
@@ -5,6 +5,22 @@ using System.IO;
 using LitJson;
 using UnityEngine.UI;
 
+public class Json_Game_Data
+{
+    public int power;
+    public int con;
+    public int hp;
+
+    public Json_Game_Data(int _power, int _con, int _hp)
+    {
+        power = _power;
+        con = _con;
+        hp = _hp;
+    }
+}
+
+
+
 public class Json_Controller : MonoBehaviour {
 
     public static Json_Controller instance = null;
@@ -17,7 +33,8 @@ public class Json_Controller : MonoBehaviour {
 
     private TextAsset json_File_1;
 
-
+    private List<Json_Game_Data> save_Data_01 = new List<Json_Game_Data>();
+    private List<Json_Game_Data> save_Data_02 = new List<Json_Game_Data>();
     private void Awake()
     {
         if (instance == null)
@@ -34,13 +51,13 @@ public class Json_Controller : MonoBehaviour {
     {
         
         // json_File_1 = File.ReadAllText(Application.dataPath + "/JHM.Assets/Resources/JsonFile01.json");
-        json_File_1 = Resources.Load<TextAsset>("JHM.Resources.Json/JsonFile01");
+        json_File_1 = Resources.Load<TextAsset>("JHM.Resources.Json/Defualt_Json_Data/Defualt_Ability");
 
         load_Data = JsonMapper.ToObject(json_File_1.text);
 
-        Json_Parsing_Data(load_Data);
+        Defualt_Json_Parsing_Data(load_Data);
     }
-    private void Json_Parsing_Data(JsonData data)
+    private void Defualt_Json_Parsing_Data(JsonData data)
     {
         text_Condition[0].text = data[0]["START_STR"].ToString();
         text_Condition[1].text = data[0]["START_CON"].ToString(); 
@@ -79,8 +96,48 @@ public class Json_Controller : MonoBehaviour {
         text_Condition[2].text = i.ToString();
     }
     // ----------------------------------
+    
 
+    public void New_Json_Data_Saving_Button_Pressed()
+    {
+        int temp = int.Parse(Game_Controller.instance.text_Save_Location_Number.text.ToString());
+        switch (temp)
+        {
+            case 1:
+                {
+                    Json_Data_Save_Area_Number_1();
+                } break;
+            case 2:
+                {
+                    Json_Data_Save_Area_Number_2();
+                }
+                break;
+        }       
+    }
+    private void Json_Data_Save_Area_Number_1()
+    {
+        save_Data_01.Add(new Json_Game_Data(int.Parse(text_Condition[0].text.ToString()),
+            int.Parse(text_Condition[1].text.ToString()),
+            int.Parse(text_Condition[2].text.ToString())));
 
+        JsonData to_Json = JsonMapper.ToJson(save_Data_01);
+
+        File.WriteAllText(Application.dataPath + "/Resources/JHM.Resources.Json/save_01.json", to_Json.ToString());
+
+        Debug.Log(to_Json.ToString());
+    }
+    private void Json_Data_Save_Area_Number_2()
+    {
+        save_Data_02.Add(new Json_Game_Data(int.Parse(text_Condition[0].text.ToString()),
+            int.Parse(text_Condition[1].text.ToString()),
+            int.Parse(text_Condition[2].text.ToString())));
+
+        JsonData to_Json = JsonMapper.ToJson(save_Data_02);
+
+        File.WriteAllText(Application.dataPath + "/Resources/JHM.Resources.Json/save_02.json", to_Json.ToString());
+
+        Debug.Log(to_Json.ToString());
+    }
 } // class
 
 
