c71199af3362f17e5ed02de621c763ead02923fc
DESKTOP-1CKJN7Q\jeong <hyeongman88@gmail.com>
2018-06-21 16:39:37 +0900

    형만_6주차_프로젝트_02

--- Project_Pri/Assets/JHM.Assets/JHM_Scripts/Staters_Window/Game_Controller.cs
+++ Project_Pri/Assets/JHM.Assets/JHM_Scripts/Staters_Window/Game_Controller.cs
@@ -1,12 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Game_Controller : MonoBehaviour {
 
-	
+    public static Game_Controller instance = null;
+
+    public GameObject origin_Panel;
+    public GameObject condition_Panel;
 
 
+    private int count_One;
+    
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void Start()
+    {
+        origin_Panel.SetActive(true);
+        condition_Panel.SetActive(false);
+
+        // -------------------------
+
+        count_One = 1; 
+    }
+    // Condition_Panel에 있는 능력치 조절
+    public int Add_One(int temp)
+    {
+        temp += count_One;
+
+        return temp;
+    }
+    public int Sub_One(int temp)
+    {
+        temp -= count_One;
+
+        return temp;
+    }
+    // ---------------
+
+    
+    public void Clicked_Button_Condition_Panel()
+    {
+        origin_Panel.SetActive(false);
+        condition_Panel.SetActive(true);
+
+        // -----------------------
+
+        Json_Controller.instance.Defualt_Json_Data();
+    }
+    
 
 
 
--- Project_Pri/Assets/JHM.Assets/JHM_Scripts/Staters_Window/Json_Controller.cs
+++ Project_Pri/Assets/JHM.Assets/JHM_Scripts/Staters_Window/Json_Controller.cs
@@ -7,9 +7,8 @@ using UnityEngine.UI;
 
 public class Json_Controller : MonoBehaviour {
 
-    public GameObject origin_Panel;
-    public GameObject condition_Panel;
-
+    public static Json_Controller instance = null;
+    
     public Text[] text_Condition;
     
     private JsonData load_Data;
@@ -17,35 +16,71 @@ public class Json_Controller : MonoBehaviour {
     //private string json_File_1;
 
     private TextAsset json_File_1;
-    private void Start()
+
+
+    private void Awake()
     {
-        origin_Panel.SetActive(true);
-        condition_Panel.SetActive(false);
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);
+        }
     }
-
-    public void Load_Json_Files()
+    // Condition_Panel에 있는 능력치 초기화
+    public void Defualt_Json_Data()
     {
-        origin_Panel.SetActive(false);
-        condition_Panel.SetActive(true);
-
-        // --------------------------
-
-        // json_File_1 = File.ReadAllText(Application.dataPath + "/JHM.Assets/Resources/JsonFile01.json");\
+        
+        // json_File_1 = File.ReadAllText(Application.dataPath + "/JHM.Assets/Resources/JsonFile01.json");
         json_File_1 = Resources.Load<TextAsset>("JHM.Resources.Json/JsonFile01");
 
         load_Data = JsonMapper.ToObject(json_File_1.text);
 
         Json_Parsing_Data(load_Data);
     }
-    
     private void Json_Parsing_Data(JsonData data)
     {
         text_Condition[0].text = data[0]["START_STR"].ToString();
         text_Condition[1].text = data[0]["START_CON"].ToString(); 
         text_Condition[2].text = data[0]["START_HP"].ToString();
     }
+    // ---------------------
+    // Condition_Panel에 있는 능력치 조절 control
+    public void Power_Plus_Pressed()
+    {
+        int i = Game_Controller.instance.Add_One(int.Parse(text_Condition[0].text.ToString()));
+        text_Condition[0].text = i.ToString();
+    }
+    public void Power_Minus_Pressed()
+    {
+        int i = Game_Controller.instance.Sub_One(int.Parse(text_Condition[0].text.ToString()));
+        text_Condition[0].text = i.ToString();
+    }
+    public void Con_Plus_Pressed()
+    {
+        int i = Game_Controller.instance.Add_One(int.Parse(text_Condition[1].text.ToString()));
+        text_Condition[1].text = i.ToString();
+    }
+    public void Con_Minus_Pressed()
+    {
+        int i = Game_Controller.instance.Sub_One(int.Parse(text_Condition[1].text.ToString()));
+        text_Condition[1].text = i.ToString();
+    }
+    public void HP_Plus_Pressed()
+    {
+        int i = Game_Controller.instance.Add_One(int.Parse(text_Condition[2].text.ToString()));
+        text_Condition[2].text = i.ToString();
+    }
+    public void HP_Minus_Pressed()
+    {
+        int i = Game_Controller.instance.Sub_One(int.Parse(text_Condition[2].text.ToString()));
+        text_Condition[2].text = i.ToString();
+    }
+    // ----------------------------------
 
-    
+
 } // class
 
 
