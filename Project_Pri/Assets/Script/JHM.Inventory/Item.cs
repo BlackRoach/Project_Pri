d12b4facb154ce083574364dd18a2c448e6c6d17
DESKTOP-7U2HS85\jeong <hyeongman88@gmail.com>
2018-07-02 16:30:24 +0900

    형만_7주차_프로젝트_03

---
+++ Project_Pri/Assets/Script/JHM.Inventory/Item.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+
+    public bool is_Dragging;
+
+
+    private void Start()
+    {
+        is_Dragging = false;
+    }
+
+    
+    public void Is_Clicked()
+    {
+        if (!is_Dragging)
+            is_Dragging = true;
+
+        if (is_Dragging)
+        {
+            this.transform.parent = this.transform.parent.parent.parent;
+        }
+
+    }
+
+
+    public void Click_Released()
+    {
+        if (is_Dragging)
+        {
+            is_Dragging = false;
+        }
+    }
+    
+} // class
+
+
+
+
+
+
+
+
+
+
