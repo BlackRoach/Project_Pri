21c99817888b4ce3dc01cb76330500b76fec1d78
bluepipimi <bluepipimi@naver.com>
2018-07-18 19:46:42 +0900

    인앱결제 구현하고 테스트 해보았습니다.

--- Project_Pri/Assets/Script/Shop/Shop.cs
+++ Project_Pri/Assets/Script/Shop/Shop.cs
@@ -6,24 +6,31 @@ using UnityEngine.SceneManagement;
 
 public class Shop : MonoBehaviour {
 
-    public GameObject SlotPrefab;
+    public GameObject SlotPrefab; // 일반 아이템 슬롯
+    public GameObject CashItemSlotPrefab; // 과금 아이템 슬롯
+    public GameObject SpendCashSlotPrefab; // 과금 아이템 사용 슬롯
     public GameObject Shop1Panel;
     public GameObject Shop2Panel;
+    public GameObject Shop3Panel;
     public GameObject Shop1ScrollRect;
     public GameObject Shop2ScrollRect;
+    public GameObject Shop3ScrollRect;
     public GameObject Shop1Label;
     public GameObject Shop2Label;
+    public GameObject Shop3Label;
     public Scrollbar Scrollbar1;
     public Scrollbar Scrollbar2;
+    public Scrollbar Scrollbar3;
 
     private Item_Json_DataBase item_Database;
 
-    public static int money = 2000; // Static 변수는 씬이 바껴도 값이 유지된다.
+    private static int money = 2000; // Static 변수는 씬이 바껴도 값이 유지된다.
+    private static int diamond = 0; 
     public Text moneyTxt;
+    public Text diamondTxt;
     public GameObject DialoguePanelAskIfBuy;
     public GameObject DialoguePanelInvenFull;
     private int price = 0;
-    private int id = 0;
 
     private Items_Info itemInfo;
 
@@ -52,8 +59,10 @@ public class Shop : MonoBehaviour {
             AddItem(30001, Shop2ScrollRect);
         }
 
-
+        AddCashItem();
+        AddItemSpendingDia();
         moneyTxt.text = "보유골드 : " + money.ToString();
+        diamondTxt.text = "다이아 : " + diamond.ToString();
 	}
 	
 	// Update is called once per frame
@@ -81,11 +90,11 @@ public class Shop : MonoBehaviour {
         Shop2ScrollRect.GetComponent<ScrollRect>().horizontalNormalizedPosition = Scrollbar2.value;
     }
 
-    private void AddItem(int _id, GameObject ShopPanel)
+    private void AddItem(int _id, GameObject ShopScrollRect)
     {
         Items_Info add_Item = item_Database.Search_For_Item(_id);
-        GameObject tmp = Instantiate(SlotPrefab);
-        tmp.transform.SetParent(ShopPanel.transform);
+        GameObject tmp = Instantiate(SlotPrefab, ShopScrollRect.transform);
+        //tmp.transform.SetParent(ShopPanel.transform); // 안드로이드폰에서 슬롯이 작아지는 문제가 생김
         ShopSlot slot = tmp.GetComponent<ShopSlot>();
         //Debug.Log(add_Item.name);
         slot.nameTxt.text = add_Item.name; // 이름 입력
@@ -93,6 +102,28 @@ public class Shop : MonoBehaviour {
         slot.infoTxt.text = add_Item.description; // 설명 입력
         slot.itemImage.sprite = add_Item.item_Img; // 이미지 입력
         slot.id = _id; // ID 입력
+    }
+
+    // 현금 구매 아이템 추가 (다이아 추가 아이템)
+    private void AddCashItem()
+    {
+        GameObject tmp = Instantiate(CashItemSlotPrefab, Shop3ScrollRect.transform);
+        ShopSlot slot = tmp.GetComponent<ShopSlot>();
+        slot.nameTxt.text = "다이아10";
+        slot.priceTxt.text = "현금 : 1000원";
+        slot.infoTxt.text = "유료아이템을 구입하기 위한 다이아10개";
+        slot.itemImage.color = new Color(0, 0, 0, 0);
+    }
+
+    // 다이아 소비 아이템 추가
+    private void AddItemSpendingDia()
+    {
+        GameObject tmp = Instantiate(SpendCashSlotPrefab, Shop3ScrollRect.transform);
+        ShopSlot slot = tmp.GetComponent<ShopSlot>();
+        slot.nameTxt.text = "다이아 삭감";
+        slot.priceTxt.text = "다이아 1개";
+        slot.infoTxt.text = "다이아 1개 삭감";
+        slot.itemImage.color = new Color(0, 0, 0, 0);
     }
 
     public void BuyItem(int _id)
@@ -112,8 +143,6 @@ public class Shop : MonoBehaviour {
         }
         this.price = price;
         DialoguePanelAskIfBuy.SetActive(true);
-
-        //Debug.Log("Hi");
     }
 
     public void Yes()
@@ -135,6 +164,20 @@ public class Shop : MonoBehaviour {
         DialoguePanelInvenFull.SetActive(false);
     }
 
+    public void AddDiamond()
+    {
+        diamond += 10;
+        diamondTxt.text = "다이아 : " + diamond;
+    }
+
+    public void SpendDia()
+    {
+        if(diamond > 0)
+            diamond--;
+        diamondTxt.text = "다이아 : " + diamond;
+
+    }
+
     public void SceneChange()
     {
         SceneManager.LoadScene("Shop_Result");
@@ -144,18 +187,32 @@ public class Shop : MonoBehaviour {
     {
         Shop1Panel.SetActive(true);
         Shop2Panel.SetActive(false);
+        Shop3Panel.SetActive(false);
         Shop1Label.GetComponent<Image>().color = Color.yellow;
         Shop2Label.GetComponent<Image>().color = Color.white;
+        Shop3Label.GetComponent<Image>().color = Color.white;
     }
 
     public void ActivateShop2()
     {
         Shop1Panel.SetActive(false);
         Shop2Panel.SetActive(true);
+        Shop3Panel.SetActive(false);
         Shop1Label.GetComponent<Image>().color = Color.white;
         Shop2Label.GetComponent<Image>().color = Color.yellow;
+        Shop3Label.GetComponent<Image>().color = Color.white;
     }
 
-    
+    public void ActivateShop3()
+    {
+        Shop1Panel.SetActive(false);
+        Shop2Panel.SetActive(false);
+        Shop3Panel.SetActive(true);
+        Shop1Label.GetComponent<Image>().color = Color.white;
+        Shop2Label.GetComponent<Image>().color = Color.white;
+        Shop3Label.GetComponent<Image>().color = Color.yellow;
+    }
+
+
 
 }
--- Project_Pri/Assets/Script/Shop/ShopSlot.cs
+++ Project_Pri/Assets/Script/Shop/ShopSlot.cs
@@ -14,15 +14,33 @@ public class ShopSlot : MonoBehaviour {
     public int id;
    
     private Shop shop;
+    private IAPManager iapManager;
 
     // Prefab을 Instantiate 하고 shop 클래스를 연결해준다.
     public void Start()
     {
-        shop = GameObject.Find("Shop").GetComponent<Shop>();   
+        shop = GameObject.Find("Shop").GetComponent<Shop>();
+        iapManager = GameObject.Find("IAPManager").GetComponent<IAPManager>();
     }
 
     public void BuyItem()
     {
         shop.BuyItem(id);
     }
+
+    public void BuyCashItem()
+    {
+        iapManager.BuyConsumable();
+    }
+
+    public void SpendDia()
+    {
+        shop.SpendDia();
+    }
 }
+
+// 시작시에 과금 아이템 슬롯을 생성한다.
+
+// 과금 아이템 슬롯을 클릭한다.
+// 다이아가 10 증가한다.
+
