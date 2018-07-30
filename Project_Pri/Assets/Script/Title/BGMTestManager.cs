ee82c9051106b252550dbcb775bb682691c40fb5
bluepipimi <bluepipimi@naver.com>
2018-07-30 16:58:31 +0900

    사운드 기능 추가

---
+++ Project_Pri/Assets/Script/Title/BGMTestManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BGMTestManager : MonoBehaviour {
+
+    public List<AudioClip> clipLists;
+
+    public void GoToTitle()
+    {
+        SceneManager.LoadScene("Title 1");
+    }
+
+    public void SoundEffectTest(AudioClip clip)
+    {
+        AudioManager AM = FindObjectOfType<AudioManager>();
+        AM.SFPlay(clip);
+    }
+
+    public void PlayNextBGM()
+    {
+        AudioManager AM = FindObjectOfType<AudioManager>();
+
+        // 현재 재생되고 있는 노래를 리스트에서 찾는다.
+        // 그 다음에 있는 노래를 재생 한다.
+        // 근데 현재 재생되고 있는 노래가 리스트 맨 마지막이라면
+        // 처음에 있는 노래을 재생한다.
+        for (int i =0; i < clipLists.Count; i++)
+        {
+            if(clipLists[i].name == AM.BGM.clip.name)
+            {
+                
+                if (i == clipLists.Count - 1)  
+                {
+                    AM.BGMPlay(clipLists[0]);
+                }
+                else
+                {
+                    AM.BGMPlay(clipLists[i + 1]);
+                }
+                return;
+            }
+        }
+    }
+}
---
+++ Project_Pri/Assets/Script/Title/Option.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Option : MonoBehaviour {
+
+    [SerializeField] private GameObject optionPanel;
+    [SerializeField] private GameObject settingPanel;
+    [SerializeField] private GameObject supportPanel;
+    [SerializeField] private Slider BGMVolume;
+    [SerializeField] private Slider SFVolume;
+
+    private void Start()
+    {
+        SetSliderValues();
+    }
+
+    public void SetBGMVolume(float value)
+    {
+        AudioManager AM = FindObjectOfType<AudioManager>();
+        AM.SetBGMVolume(value);
+    }
+
+    public void SetSoundEffectVolume(float value)
+    {
+        AudioManager AM = FindObjectOfType<AudioManager>();
+        AM.SetSoundEffectVolume(value);
+    }
+
+    public void ExitOption()
+    {
+        optionPanel.SetActive(false);
+    }
+
+    public void EnterSupportPanel()
+    {
+        settingPanel.SetActive(false);
+        supportPanel.SetActive(true);
+    }
+
+    public void ExitSupportPanel()
+    {
+        supportPanel.SetActive(false);
+        settingPanel.SetActive(true);
+
+    }
+
+    // BGM과 효과음의 볼륨값을 BGM 슬라이더와 효과음 슬라이더 값에 입력합니다.
+    private void SetSliderValues()
+    {
+        AudioManager AM = FindObjectOfType<AudioManager>();
+        BGMVolume.value = AM.BGM.volume;
+        SFVolume.value = AM.SoundEffect.volume;
+    }
+}
