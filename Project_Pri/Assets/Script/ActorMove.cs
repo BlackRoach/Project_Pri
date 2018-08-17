ea9b3a099dca2f55c8884e0daf00b54ba1e548cf
bluepipimi <bluepipimi@naver.com>
2018-08-17 16:27:49 +0900

    캐릭터 타일맵 이동 및 상호작용 추가 타일맵에 충돌체 추가

---
+++ Project_Pri/Assets/Script/ActorMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorMove : MonoBehaviour {
+
+    public float moveSpeed = 3f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Move(Vector3 moveDirection)
+    {
+        moveDirection = moveDirection.normalized; // 크기가 1이상인 벡터를 무조건 1로 만든다.
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+    }
+}
+
+// <캐릭터(액터) 이동 처리>
+// - 버튼으로 처리하면 누르는 순간만 잠깐 움직인다.
---
+++ Project_Pri/Assets/Script/ArrowButton.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ArrowButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler{
+
+    private bool bMovable;
+
+    public ActorMove mainActor;
+
+    public Vector3 moveDirection;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        //Debug.Log("OnPointerDown"); // 게임오브젝트 범위 안에서 마우스 버튼을 눌렀을 때
+        bMovable = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        //Debug.Log("OnPointerUp"); // 게임오브젝트 범위 안에서 마우스 버튼을 땠을 때
+        bMovable = false;
+    }
+
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //Debug.Log("OnPointerExit"); // 게임오브젝트 범위 안에서 나갔을 때
+        bMovable = false;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(bMovable)
+        {
+            mainActor.Move(moveDirection);
+        }
+	}
+
+
+}
