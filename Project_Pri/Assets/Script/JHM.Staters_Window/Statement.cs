434cb6e1de3d7346a8c693eae9b7f81182894b44
DESKTOP-2F6UHDE\jeong <hyeongman88@gmail.com>
2018-09-02 04:38:38 +0900

    형만_16주차_프로젝트_01

---
+++ Project_Pri/Assets/Script/JHM.Staters_Window/Statement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Statement {
+
+    public int s_muscular_strength;
+    public int s_magic_power;
+    public int s_intellect;
+    public int s_charm;
+    public int s_sense;
+    public int s_reliability;
+    public int s_pride;
+    public int s_stress;
+    public int s_artistic;
+    public int s_elegance;
+    public int s_morality;
+    public int s_stamina;
+    public int s_offense_power;
+    public int s_armor;
+    public int s_magic_attack_power;
+    public int s_magical_resistance;
+    public float s_attack_speed;
+    public int s_full_hp;
+    public int s_full_mp;
+
+}
