fbf19f885c8a9f490a5c5fea0f213ea2eb3f73fc
DESKTOP-7OFEFU8\jeong <hyeongman88@gmail.com>
2018-10-03 04:25:32 +0900

    형만_19주차_프로젝트_04

--- Project_Pri/Assets/Script/JHM.Battle_Scene/Battle_Manager.cs
+++ Project_Pri/Assets/Script/JHM.Battle_Scene/Battle_Manager.cs
@@ -7,9 +7,14 @@ using System.IO;
 
 public class Battle_Manager : MonoBehaviour {
 
+    // 케릭터 위치와 이미지 오브젝트
+    public GameObject main_Character_Pos;
+    public GameObject target_Character_Pos; 
+    // 케릭터 능력치 value
+    public GameObject main_Character_Value;
+    public GameObject target_Character_Value;
 
-    public GameObject main_Character;
-    public GameObject target_Character;
+    public Text text_Announcement;
 
     private JsonData battle_Data;
 
@@ -18,14 +23,24 @@ public class Battle_Manager : MonoBehaviour {
     [SerializeField]
     private int mainChar_Muscularstrength; // 주인공 근력값
 
+    private Vector3 current_Pos_1;
+    private Vector3 current_Pos_2;
+
     private void Start()
     {
         gambling = 0;
         Defualt_Json_Battle_Data_Parsing();
 
         mainChar_Muscularstrength = PlayerPrefs.GetInt("s_muscular_strength");
+        // 케릭터 이미지 빈공간  실행시
+        main_Character_Pos.transform.GetChild(0).GetComponent<Image>().sprite = null;
+        target_Character_Pos.transform.GetChild(0).GetComponent<Image>().sprite = null;
+
     }
 
+    private void Update()
+    { 
+    }
 
     private void Defualt_Json_Battle_Data_Parsing()
     {
@@ -42,20 +57,37 @@ public class Battle_Manager : MonoBehaviour {
         gambling = temp;
         Set_Sparring_Character();
         Set_Main_Character();
+
+        // -------------------------------
+
+        target_Character_Pos.transform.GetChild(0).GetComponent<Image>().sprite = Resources.Load<Sprite>("JHM.Img/" +
+            battle_Data[gambling - 1]["CHARACTER_SD"]);
+
+        target_Character_Pos.transform.GetChild(0).transform.localScale = new Vector3(-1f, 1f, 1f);
+
+        main_Character_Pos.transform.GetChild(0).GetComponent<Image>().sprite = Resources.Load<Sprite>("JHM.Img/" +
+            "character_sd_eileen");
     }
 
     private void Set_Sparring_Character()
     {
-        target_Character.transform.GetChild(0).GetComponent<Text>().text = battle_Data[gambling - 1]["NAME"].ToString();
-        target_Character.transform.GetChild(1).GetComponent<Text>().text = battle_Data[gambling - 1]["STATE"].ToString();
-        target_Character.transform.GetChild(2).GetComponent<Text>().text = battle_Data[gambling - 1]["STATE_BASIC"].ToString();
-        target_Character.transform.GetChild(3).GetComponent<Text>().text = battle_Data[gambling - 1]["STATE_PLUS"].ToString();
+        target_Character_Value.transform.GetChild(0).GetComponent<Text>().text = battle_Data[gambling - 1]["NAME"].ToString();
+        target_Character_Value.transform.GetChild(1).GetComponent<Text>().text = battle_Data[gambling - 1]["STATE"].ToString();
+        target_Character_Value.transform.GetChild(2).GetComponent<Text>().text = battle_Data[gambling - 1]["STATE_BASIC"].ToString();
+        target_Character_Value.transform.GetChild(3).GetComponent<Text>().text = battle_Data[gambling - 1]["STATE_PLUS"].ToString();
     }
     private void Set_Main_Character()
     {
-        main_Character.transform.GetChild(1).GetComponent<Text>().text = mainChar_Muscularstrength.ToString();
-        main_Character.transform.GetChild(2).GetComponent<Text>().text = 10.ToString();
-        main_Character.transform.GetChild(3).GetComponent<Text>().text = 10.ToString();
+        main_Character_Value.transform.GetChild(1).GetComponent<Text>().text = mainChar_Muscularstrength.ToString();
+        main_Character_Value.transform.GetChild(2).GetComponent<Text>().text = 10.ToString();
+        main_Character_Value.transform.GetChild(3).GetComponent<Text>().text = 10.ToString();
+    }
+    // -------------------
+    public void Button_Sparring_Is_Start()
+    {
+        text_Announcement.text = battle_Data[gambling - 1]["START_TEXT"].ToString();
+
+
     }
 
 
