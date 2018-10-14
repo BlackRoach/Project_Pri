ba45d58c355398c27eaf2ca5125e0a0e5c628de0
BlackRoach <black4roach@gmail.com>
2018-10-14 17:46:31 +0900

    add tile graphics

---
+++ Project_Pri/Assets/Script/World_Scripts/TileMapInformation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMapInformation : MonoBehaviour {
+
+   [SerializeField] private InGamemanager inGamemanager;
+   
+    public Transform[] spawnpos;
+ 
+    private void Start()
+    {
+        inGamemanager = InGamemanager.Instance;
+    }
+
+    public void SpawnPlayer(int i)
+    {
+       GameObject.Find("@Player").gameObject.transform.position = spawnpos[i].position;
+    }
+  
+}
