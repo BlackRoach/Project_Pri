38aa0fac05d0d8b58d13902165fe1ab9d432910d
BlackRoach <black4roach@gmail.com>
2018-10-14 17:55:49 +0900

    add graphic 6

---
+++ Project_Pri/Assets/Script/World_Scripts/MoveArea.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveArea : MonoBehaviour {
+
+    public int tilenum;
+
+    private TileMapLoader tileMapLoader;
+	
+	void Start () {
+        tileMapLoader = TileMapLoader.Instance;	
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            tileMapLoader.ChangeMap(tilenum);
+        }
+
+    }
+
+}
