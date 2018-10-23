826f47f4ecc9c61ac3435c2cc69dba7afd455e44
DESKTOP-5IMDRL9\jeong <hyeongman88@gmail.com>
2018-10-24 01:22:02 +0900

    형만_22주차_프로젝트_02

---
+++ Project_Pri/Assets/Script/JHM.Schedule/Market_Event_One.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Market_Event_One : MonoBehaviour {
+    public GameObject[] sd_Character;
+    public Transform[] sd_Pos;
+    private Vector3[] New_Pos = new Vector3[3];
+
+    private void Start()
+    {
+        sd_Character[0].transform.localPosition = sd_Pos[0].localPosition;
+        New_Pos[0] = sd_Pos[2].localPosition;
+        sd_Character[1].transform.localPosition = sd_Pos[1].localPosition;
+        New_Pos[1] = sd_Pos[2].localPosition;
+        sd_Character[3].transform.localPosition = sd_Pos[5].localPosition;
+        New_Pos[2] = sd_Pos[4].localPosition;
+    }
+
+    private void Update()
+    {
+        StartCoroutine(Character_Move_Toward_Root());
+    }
+    // market_1_root
+    IEnumerator Character_Move_Toward_Root()
+    {
+        sd_Character[0].transform.localPosition = Vector3.MoveTowards(sd_Character[0].transform.localPosition,
+            New_Pos[0], 2f);
+        sd_Character[1].transform.localPosition = Vector3.MoveTowards(sd_Character[1].transform.localPosition,
+            New_Pos[1], 2f);
+        sd_Character[3].transform.localPosition = Vector3.MoveTowards(sd_Character[3].transform.localPosition,
+            New_Pos[2], 2f);
+        yield return new WaitForSeconds(0f);
+        if (sd_Character[0].transform.localPosition.x == sd_Pos[2].localPosition.x &&
+           sd_Character[0].transform.localPosition.y == sd_Pos[2].localPosition.y)
+        {
+            New_Pos[0] = sd_Pos[5].localPosition;
+        }
+        if (sd_Character[1].transform.localPosition.x == sd_Pos[2].localPosition.x &&
+           sd_Character[1].transform.localPosition.y == sd_Pos[2].localPosition.y)
+        {
+            New_Pos[1] = sd_Pos[1].localPosition;
+        }
+        if (sd_Character[3].transform.localPosition.x == sd_Pos[4].localPosition.x &&
+           sd_Character[3].transform.localPosition.y == sd_Pos[4].localPosition.y)
+        {
+            New_Pos[2] = sd_Pos[2].localPosition;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} // class
+
+
+
+
+
+
+
---
+++ Project_Pri/Assets/Script/JHM.Schedule/Market_Event_Three.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Market_Event_Three : MonoBehaviour {
+
+    public GameObject[] sd_Character;
+    public Transform[] sd_Pos;
+
+    private Vector3[] NewPos = new Vector3[3];
+
+    private void Start()
+    {
+        sd_Character[0].transform.localPosition = sd_Pos[0].localPosition;
+        NewPos[0] = sd_Pos[2].localPosition;
+
+        NewPos[1] = sd_Pos[2].localPosition;
+
+        sd_Character[4].transform.localPosition = sd_Pos[5].localPosition;
+        NewPos[2] = sd_Pos[2].localPosition;
+    }
+
+    private void Update()
+    {
+        StartCoroutine(Character_Move_Toward_Root());
+    }
+
+
+    IEnumerator Character_Move_Toward_Root()
+    {
+        sd_Character[0].transform.localPosition = Vector3.MoveTowards(sd_Character[0].transform.localPosition,
+            NewPos[0], 2f);
+        sd_Character[2].transform.localPosition = Vector3.MoveTowards(sd_Character[2].transform.localPosition,
+            NewPos[1], 2f);
+        sd_Character[4].transform.localPosition = Vector3.MoveTowards(sd_Character[4].transform.localPosition,
+            NewPos[2], 2f);
+        yield return new WaitForSeconds(0f);
+
+        if(sd_Character[0].transform.localPosition.x == sd_Pos[2].localPosition.x &&
+            sd_Character[0].transform.localPosition.y == sd_Pos[2].localPosition.y)
+        {
+            NewPos[0] = sd_Pos[5].localPosition;
+        }
+        if (sd_Character[2].transform.localPosition.x == sd_Pos[2].localPosition.x &&
+            sd_Character[2].transform.localPosition.y == sd_Pos[2].localPosition.y)
+        {
+            NewPos[1] = sd_Pos[0].localPosition;
+        }
+        if (sd_Character[4].transform.localPosition.x == sd_Pos[2].localPosition.x &&
+            sd_Character[4].transform.localPosition.y == sd_Pos[2].localPosition.y)
+        {
+            NewPos[2] = sd_Pos[1].localPosition;
+        }
+    }
+
+
+
+
+} // class
+
+
+
+
+
+
+
