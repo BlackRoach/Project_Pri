92281d3cd38f9feeaf8247d8496e58173c28f8a1
DESKTOP-97VQ0AH\jeong <hyeongman88@gmail.com>
2018-11-21 03:41:43 +0900

    형만_26주차_프로젝트_03

---
+++ Project_Pri/Assets/Script/JHM.Schedule/Play_Talk_Event.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+using System.IO;
+
+
+public class Play_Talk_Event : MonoBehaviour {
+
+
+    public static Play_Talk_Event instance = null;
+
+    public Schedule_Each_Count event_List = new Schedule_Each_Count();  // 대화 이벤트 횟수
+
+    private JsonData play_Count_Data;
+    private JsonData trigger_List_Data;
+    private JsonData event_List_Data;
+    private JsonData dialog_List_Data;
+    private JsonData select_List_Data;
+    
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void Start()
+    {
+        Json_Data_All_Parsing();
+    }
+    private void Json_Data_All_Parsing()
+    {
+        TextAsset json_File_1 = Resources.Load<TextAsset>("JHM.Resources.Json/Defualt_Json_Data/PLAY_COUNT_DATA");
+        play_Count_Data = JsonMapper.ToObject(json_File_1.text);
+        TextAsset json_File_2 = Resources.Load<TextAsset>("JHM.Resources.Json/Defualt_Json_Data/TRIGGER_LIST");
+        trigger_List_Data = JsonMapper.ToObject(json_File_2.text);
+        TextAsset json_File_3 = Resources.Load<TextAsset>("JHM.Resources.Json/Defualt_Json_Data/EVENT_LIST_1");
+        event_List_Data = JsonMapper.ToObject(json_File_3.text);
+        TextAsset json_File_4 = Resources.Load<TextAsset>("JHM.Resources.Json/Defualt_Json_Data/DIALOG_LIST_1");
+        dialog_List_Data = JsonMapper.ToObject(json_File_4.text);
+        TextAsset json_File_5 = Resources.Load<TextAsset>("JHM.Resources.Json/Defualt_Json_Data/SELECT_LIST");
+        select_List_Data = JsonMapper.ToObject(json_File_5.text);
+    }
+} // class
+
+
+
+
+
+
+
+
---
+++ Project_Pri/Assets/Script/JHM.Schedule/Schedule_Each_Count.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Schedule_Each_Count  {
+
+    private int _MAGIC_P;
+    private int _MILITARY_P;
+    private int _COOKING_P;
+    private int _MANNERS_P;
+    private int _SCHOOL_P;
+    private int _MUSIC_P;
+    private int _DANCING_P;
+    private int _PICTURE_P;
+    private int _MONASTERY_P;
+    private int _HUNTER_P;
+    private int _PUB_P;
+    private int _TUTOR_P;
+    private int _BARD_P;
+    private int _FARM_P;
+    private int _CATHEDRAL_P;
+    private int _GRAVEYARD_P;
+    private int _MARKET_P;
+    private int _MINE_P;
+    private int _MAID_P;
+    private int _HOUSEWORK_P;
+    private int _THEATER_P;
+    private int _FREE_P;
+
+    // -------------------------
+    public Schedule_Each_Count()
+    {
+        _MAGIC_P = 0;
+        _MILITARY_P = 0;
+        _COOKING_P = 0;
+        _MANNERS_P = 0;
+        _SCHOOL_P = 0;
+        _MUSIC_P = 0;
+        _DANCING_P = 0;
+        _PICTURE_P = 0;
+        _MONASTERY_P = 0;
+        _HUNTER_P = 0;
+        _PUB_P = 0;
+        _TUTOR_P = 0;
+        _BARD_P = 0;
+        _FARM_P = 0;
+        _CATHEDRAL_P = 0;
+        _GRAVEYARD_P = 0;
+        _MARKET_P = 0;
+        _MINE_P = 0;
+        _MAID_P = 0;
+        _HOUSEWORK_P = 0;
+        _THEATER_P = 0;
+        _FREE_P = 0;
+    }
+
+    public int MAGIC_P
+    {
+        get { return _MAGIC_P; }
+        set { _MAGIC_P = value; }
+    }
+    public int MILITARY_P
+    {
+        get { return _MILITARY_P; }
+        set { _MILITARY_P = value; }
+    }
+    public int COOKING_P
+    {
+        get { return _COOKING_P; }
+        set { _COOKING_P = value; }
+    }
+    public int MANNERS_P
+    {
+        get { return _MANNERS_P; }
+        set { _MANNERS_P = value; }
+    }
+    public int SCHOOL_P
+    {
+        get { return _SCHOOL_P; }
+        set { _SCHOOL_P = value; }
+    }
+    public int MUSIC_P
+    {
+        get { return _MUSIC_P; }
+        set { _MUSIC_P = value; }
+    }
+    public int DANCING_P
+    {
+        get { return _DANCING_P; }
+        set { _DANCING_P = value; }
+    }
+    public int PICTURE_P
+    {
+        get { return _PICTURE_P; }
+        set { _PICTURE_P = value; }
+    }
+    public int MONASTERY_P
+    {
+        get { return _MONASTERY_P; }
+        set { _MONASTERY_P = value; }
+    }
+    public int HUNTER_P
+    {
+        get { return _HUNTER_P; }
+        set { _HUNTER_P = value; }
+    }
+    public int PUB_P
+    {
+        get { return _PUB_P; }
+        set { _PUB_P = value; }
+    }
+    public int TUTOR_P
+    {
+        get { return _TUTOR_P; }
+        set { _TUTOR_P = value; }
+    }
+    public int BARD_P
+    {
+        get { return _BARD_P; }
+        set { _BARD_P = value; }
+    }
+    public int FARM_P
+    {
+        get { return _FARM_P; }
+        set { _FARM_P = value; }
+    }
+    public int CATHEDRAL_P
+    {
+        get { return _CATHEDRAL_P; }
+        set { _CATHEDRAL_P = value; }
+    }
+    public int GRAVEYARD_P
+    {
+        get { return _GRAVEYARD_P; }
+        set { _GRAVEYARD_P = value; }
+    }
+    public int MARKET_P
+    {
+        get { return _MARKET_P; }
+        set { _MARKET_P = value; }
+    }
+    public int MINE_P
+    {
+        get { return _MINE_P; }
+        set { _MINE_P = value; }
+    }
+    public int MAID_P
+    {
+        get { return _MAID_P; }
+        set { _MAID_P = value; }
+    }
+    public int HOUSEWORK_P
+    {
+        get { return _HOUSEWORK_P; }
+        set { _HOUSEWORK_P = value; }
+    }
+    public int THEATER_P
+    {
+        get { return _THEATER_P; }
+        set { _THEATER_P = value; }
+    }
+    public int FREE_P
+    {
+        get { return _FREE_P; }
+        set { _FREE_P = value; }
+    }
+} // class
