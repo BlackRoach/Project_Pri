11083a9839b23946245d9214f8c1af6d88fb48ac
DESKTOP-KMBG76A\jeong <hyeongman88@gmail.com>
2018-11-24 04:21:32 +0900

    형만_26주차_프로젝트_05

--- Project_Pri/Assets/Script/JHM.Schedule/Play_Talk_Event.cs
+++ Project_Pri/Assets/Script/JHM.Schedule/Play_Talk_Event.cs
@@ -12,7 +12,9 @@ public class Play_Talk_Event : MonoBehaviour {
     public static Play_Talk_Event instance = null;
 
     public GameObject talk_Event_Parent;
-    public Image bg_Img;
+    public GameObject bg_Img;
+    public GameObject dialog_Box;
+    public GameObject select_Box;
     // 케릭터 이미지 그리고 애니메이션 
     public GameObject left_Character;
     public GameObject middle_Character;
@@ -20,7 +22,7 @@ public class Play_Talk_Event : MonoBehaviour {
     // --------------------
     public Schedule_Each_Count event_List = new Schedule_Each_Count();  // 대화 이벤트 횟수
 
-    public Current_Talk_Event current_Event = new Current_Talk_Event();
+    public Current_Talk_Event current_Event = new Current_Talk_Event(); // 대화이벤트 실행매니저
 
     private JsonData play_Count_Data;
     private JsonData trigger_List_Data;
@@ -42,6 +44,11 @@ public class Play_Talk_Event : MonoBehaviour {
     private void Start()
     {
         talk_Event_Parent.SetActive(false);
+        bg_Img.SetActive(false);
+        left_Character.SetActive(false);
+        middle_Character.SetActive(false);
+        right_Character.SetActive(false);
+
         Json_Data_All_Parsing();
     }
     private void Json_Data_All_Parsing()
@@ -301,14 +308,222 @@ public class Play_Talk_Event : MonoBehaviour {
                     if((int)play_Count_Data[i]["TRIGGER_NOT"] != 0)
                     {
                         // 트리거 작동
-                        // 테스트 코드 
-                        talk_Event_Parent.SetActive(true);
-                        current_Event.input_Value = "school_img";
-                        BG_MAKE();
-                        Char_Make_Left();
-                        Char_Make_Right();
-                        // ---------
+                        if((int)play_Count_Data[i]["TRIGGER_NUM"] == 1)
+                        {
+                            current_Event.event_Name = event_List_Data[0]["EVENT_NAME"].ToString();
+                            current_Event.end_Event_Count = (int)event_List_Data[0]["EVENT_COUNT"];
+                            current_Event.current_Event_Count++;
+                            current_Event.event_Fuction = event_List_Data[0]["EVENT_1"].ToString();
+                            current_Event.input_Value = event_List_Data[0]["EVENT_IN_1"].ToString();
+
+                            Event_Run_Manager();
+                            break;
+                        }
                     }
+                }
+            }
+        }
+    }
+    // 대사 오토 타이핑
+    IEnumerator Auto_Typing_Dialog_Text(string str_Text)
+    {
+        dialog_Box.transform.GetChild(0).GetComponent<Text>().text = " ";
+        string _text = str_Text;
+        foreach (char input_Char in _text.ToCharArray())
+        {
+            dialog_Box.transform.GetChild(0).GetComponent<Text>().text += input_Char;
+            yield return new WaitForSeconds(0.05f);
+        }
+        dialog_Box.transform.GetChild(3).gameObject.SetActive(true);
+    }
+
+    // 이벤트 함수 모음
+    public void BG_MAKE()
+    {
+        talk_Event_Parent.SetActive(true);
+        bg_Img.SetActive(true);
+        bg_Img.GetComponent<Image>().sprite = Resources.Load<Sprite>("JHM.Img/" + current_Event.input_Value);
+        // --------------------
+        current_Event.current_Event_Count++;
+        current_Event.event_Fuction = "CHA_MAKE_L";
+        current_Event.input_Value = "marienne";
+        Event_Run_Manager();
+    }
+    public void BG_OUT()
+    {
+        bg_Img.GetComponent<Image>().sprite = null;
+        bg_Img.SetActive(false);
+        talk_Event_Parent.SetActive(false);
+        // --------------------
+        current_Event.current_Event_Count++;
+        current_Event.event_Fuction = "0";
+        current_Event.input_Value = "0";
+    }
+    public void Char_Make_Left()
+    {
+        left_Character.SetActive(true);
+        left_Character.GetComponent<Animation>().clip = left_Character.GetComponent<Animation>().GetClip("Fade_In");
+        left_Character.GetComponent<Animation>().Play();
+        // --------------------
+        current_Event.current_Event_Count++;
+        current_Event.event_Fuction = "DIALOG_MAKE";
+        current_Event.input_Value = "70001";
+        Event_Run_Manager();
+    }
+    public void Char_Make_Right()
+    {
+        right_Character.GetComponent<Animation>().clip = right_Character.GetComponent<Animation>().GetClip("Fade_In");
+        right_Character.GetComponent<Animation>().Play();
+    }
+    public void Char_Make_Middle()
+    {
+        middle_Character.GetComponent<Animation>().clip = middle_Character.GetComponent<Animation>().GetClip("Fade_In");
+        middle_Character.GetComponent<Animation>().Play();
+    }
+    public void Char_Make_LM()
+    {
+        left_Character.GetComponent<Animation>().clip = left_Character.GetComponent<Animation>().GetClip("Left_Fade_In");
+        left_Character.GetComponent<Animation>().Play();
+    }
+    public void Char_Make_RM()
+    {
+        right_Character.GetComponent<Animation>().clip = right_Character.GetComponent<Animation>().GetClip("Right_Fade_In");
+        right_Character.GetComponent<Animation>().Play();
+    }
+    public void Char_Make_MDM()
+    {
+        middle_Character.GetComponent<Animation>().clip = middle_Character.GetComponent<Animation>().GetClip("Middle_Down_Fade_In");
+        middle_Character.GetComponent<Animation>().Play();
+    }
+    public void Char_Make_LDM()
+    {
+        left_Character.GetComponent<Animation>().clip = left_Character.GetComponent<Animation>().GetClip("Left_Down_Fade_In");
+        left_Character.GetComponent<Animation>().Play();
+    }
+    public void Char_Make_RDM()
+    {
+        right_Character.GetComponent<Animation>().clip = right_Character.GetComponent<Animation>().GetClip("Right_Down_Fade_In");
+        right_Character.GetComponent<Animation>().Play();
+    }
+    public void Char_Move_To_Left()
+    {
+        left_Character.GetComponent<Animation>().clip = left_Character.GetComponent<Animation>().GetClip("Left_Move_Char");
+        left_Character.GetComponent<Animation>().Play();
+    }
+    public void Char_Move_To_Right()
+    {
+        right_Character.GetComponent<Animation>().clip = right_Character.GetComponent<Animation>().GetClip("Right_Move_Char");
+        right_Character.GetComponent<Animation>().Play();
+    }
+    public void Char_Out()
+    {
+        left_Character.SetActive(true);
+        right_Character.SetActive(true);
+        right_Character.GetComponent<Animation>().clip = right_Character.GetComponent<Animation>().GetClip("Fade_In");
+        right_Character.GetComponent<Animation>().Play();
+    }
+    public void Char_Out_Left()
+    {
+        left_Character.GetComponent<Animation>().clip = left_Character.GetComponent<Animation>().GetClip("Left_Move_Fade_Out");
+        left_Character.GetComponent<Animation>().Play();
+    }
+    public void Char_Out_Right()
+    {
+        right_Character.GetComponent<Animation>().clip = right_Character.GetComponent<Animation>().GetClip("Right_Move_Fade_Out");
+        right_Character.GetComponent<Animation>().Play();
+    }
+    public void DIALOG_MAKE()
+    {
+        dialog_Box.SetActive(true);
+        dialog_Box.transform.GetChild(3).gameObject.SetActive(false);
+        if (current_Event.input_Value == dialog_List_Data[0]["ID"].ToString())
+        {
+            StartCoroutine(Auto_Typing_Dialog_Text(dialog_List_Data[0]["DIALOG_TEXT"].ToString()));
+            dialog_Box.transform.GetChild(1).GetComponent<Text>().text = dialog_List_Data[0]["DIALOG_NAME"].ToString();
+            dialog_Box.transform.GetChild(2).transform.GetChild(0).transform.GetChild(0).GetComponent<Image>().sprite =
+                Resources.Load<Sprite>("JHM.Img/" + dialog_List_Data[0]["DIALOG_FACE"].ToString());
+        }
+        // --------------------------
+        current_Event.current_Event_Count++;
+        current_Event.event_Fuction = "DIALOG_MAKE";
+        current_Event.input_Value = "70002";
+    }
+    public void SELECT_MAKE()
+    {
+        select_Box.SetActive(true);
+    }
+    public void Character_Fade_Out()
+    {
+        left_Character.SetActive(true);
+        left_Character.GetComponent<Animation>().clip = left_Character.GetComponent<Animation>().GetClip("Fade_out");
+        left_Character.GetComponent<Animation>().Play();
+        // -----------------------------
+        current_Event.current_Event_Count++;
+        current_Event.event_Fuction = "BG_OUT";
+        current_Event.input_Value = "school_img";
+        StartCoroutine(Left_Character_Fade_Out_Done());
+    }
+    // 케릭터 fade out 끝나고 난후 false 작업
+    IEnumerator Left_Character_Fade_Out_Done()
+    {
+        yield return new WaitForSeconds(1.2f);
+        left_Character.SetActive(false);
+        Event_Run_Manager();
+    }
+    // ---------------------------
+    public void Button_Dialog_Text_Next_Or_Exit()
+    {
+        if (current_Event.current_Event_Count == 4)
+        {
+            dialog_Box.SetActive(true);
+            dialog_Box.transform.GetChild(3).gameObject.SetActive(false);
+            if (current_Event.input_Value == dialog_List_Data[1]["ID"].ToString())
+            {
+                StartCoroutine(Auto_Typing_Dialog_Text(dialog_List_Data[1]["DIALOG_TEXT"].ToString()));
+            }
+            // ------------------------
+            current_Event.current_Event_Count++;
+            current_Event.event_Fuction = "CHA_OUT";
+            current_Event.input_Value = "marienne";
+        }
+        else
+        {
+            dialog_Box.SetActive(false);
+            Event_Run_Manager();
+        }
+    }
+    // end 함수 모음
+    // 각종 이벤트 실행 매니저
+    private void Event_Run_Manager()
+    {
+        if(current_Event.event_Name == "EVENT_1")
+        {
+            if (current_Event.current_Event_Count <= current_Event.end_Event_Count)
+            {
+                switch (current_Event.event_Fuction)
+                {
+                    case "BG_MAKE":
+                        {
+                            BG_MAKE();
+                        }break;
+                    case "CHA_MAKE_L":
+                        {
+                            Char_Make_Left();
+                        }break;
+                    case "DIALOG_MAKE":
+                        {
+                            DIALOG_MAKE();
+                        }break;
+                    case "CHA_OUT":
+                        {
+                            Character_Fade_Out();
+                        }
+                        break;
+                    case "BG_OUT":
+                        {
+                            BG_OUT();
+                        }
+                        break;
                 }
             }
         }
@@ -316,23 +531,7 @@ public class Play_Talk_Event : MonoBehaviour {
 
 
 
-    // 이벤트 함수 모음
-    public void BG_MAKE()
-    {
-        bg_Img.sprite = Resources.Load<Sprite>("JHM.Img/" + current_Event.input_Value);
-    }
-    public void Char_Make_Left()
-    {
-        left_Character.GetComponent<Animation>().clip = left_Character.GetComponent<Animation>().GetClip("Fade_In");
-        left_Character.GetComponent<Animation>().Play();
-    }
-    public void Char_Make_Right()
-    {
-        right_Character.GetComponent<Animation>().clip = right_Character.GetComponent<Animation>().GetClip("Fade_In");
-        right_Character.GetComponent<Animation>().Play();
-    }
 
-    // end 함수 모음
 } // class
 
 
