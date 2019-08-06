5aecb6f48111ce9d2556c45c44362a46ce83fca9
DESKTOP-OVKFFG5\jeong <hyeongman88@gmail.com>
2019-08-07 01:55:35 +0900

    형만_구글애드몹광고_테스트완료

---
+++ Project_Pri/Assets/Script/JHM_GoogleAdmob/GoogleAdmobBanner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+
+public class GoogleAdmobBanner : MonoBehaviour {
+
+    private readonly string unitId = "ca-app-pub-3940256099942544/6300978111";
+
+    private readonly string testId = "ca-app-pub-3940256099942544/6300978111";
+
+    private readonly string testDeviceId = "33BE2250B43518CCDA7DE426D04EE231";
+
+    private BannerView banner;
+
+    private void Start()
+    {
+        RequestBanner();
+    }
+
+    private void RequestBanner()
+    {
+        string id = Debug.isDebugBuild ? testId : unitId;
+
+        banner = new BannerView(id, AdSize.SmartBanner, AdPosition.Bottom);
+
+        AdRequest request = new AdRequest.Builder().AddTestDevice(testDeviceId).Build();
+
+        banner.LoadAd(request);
+    }
+    public void ActivateBanner(bool open)
+    {
+        if (open)
+        {
+            banner.Show();
+        }else
+        {
+            banner.Hide();
+        }
+    }
+    public void DestroyBanner()
+    {
+        banner.Destroy();
+    }
+} // class
+
+
+
+
---
+++ Project_Pri/Assets/Script/JHM_GoogleAdmob/GoogleAdmobInterstitial.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class GoogleAdmobInterstitial : MonoBehaviour {
+
+    private readonly string unitId = "ca-app-pub-3940256099942544/1033173712";
+
+    private readonly string testId = "ca-app-pub-3940256099942544/1033173712";
+
+    private readonly string testDeviceId = "33BE2250B43518CCDA7DE426D04EE231";
+
+    private InterstitialAd interstitial;
+
+
+    private void Start()
+    {
+        RequestInterstital();
+        Invoke("InterstitalShow",20f);
+    }
+
+    private void RequestInterstital()
+    {
+        string id = Debug.isDebugBuild ? testId : unitId;
+
+        interstitial = new InterstitialAd(id);
+
+        AdRequest request = new AdRequest.Builder().Build();
+
+        interstitial.LoadAd(request);
+    }
+    private void InterstitalShow()
+    {
+        StartCoroutine(InterstitalLoading());
+    }
+    IEnumerator InterstitalLoading()
+    {
+        while (!interstitial.IsLoaded())
+        {
+            yield return null;
+        }
+
+        interstitial.Show();
+    }
+} // class
+
+
+
+
+
